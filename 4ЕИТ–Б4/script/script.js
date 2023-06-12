d01e26ef378a798d247a9da3d2f9975ba9a9b0ae
Lazar <lazolakivukadinovic1001@gmail.com>
2023-06-12 19:43:07 +0200

    B4 KOMENTARI

--- 4ЕИТ–Б4/script/script.js
+++ 4ЕИТ–Б4/script/script.js
@@ -1,19 +1,28 @@
+// PROMENLJIVA -> INDEX SLAJDA
 let slideIndex = 0;
 showSlides();
 
+// FUNKCIJA ZA PRIKAZIVANJE-PROMENU SLAJDOVA
 function showSlides() {
+  // PROMENLJIVE ZA KONTROLU PROMENE
   let i;
   let slides = document.getElementsByClassName("mojSlajder");
   let dots = document.getElementsByClassName("dot");
+
+  // SAKRIVANJE ODREDJENIH SLAJDOVA
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
+
+  // PRIKAZIVANJE ODREDJENIH SLAJDOVA
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+
+  // INTERVAL ZA PROMENU NA 3 SEKUNDE
+  setTimeout(showSlides, 3000);
 }
